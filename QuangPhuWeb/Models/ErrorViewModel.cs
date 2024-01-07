0f65c826c4108445ba98bff8982cdda6f8a1efb0
quangphu <trancongquangphu10@gmail.com>
2024-01-07 21:05:34 +0700

    Add project files.

---
+++ QuangPhuWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuangPhuWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
