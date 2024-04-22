a614ab2b5e4973d55dc13b49a346c50d133d151e
quangphu <trancongquangphu10@gmail.com>
2024-04-23 00:05:25 +0700

    Stripe in action 22/4

--- QuangPhuWeb/Areas/Customer/Controllers/CartController.cs
+++ QuangPhuWeb/Areas/Customer/Controllers/CartController.cs
@@ -4,6 +4,7 @@ using QuangPhu.DataAccess.Repository.IRepository;
 using QuangPhu.Models;
 using QuangPhu.Models.ViewModels;
 using QuangPhu.Utility;
+using Stripe.Checkout;
 using System.Security.Claims;
 
 namespace QuangPhuWeb.Areas.Customer.Controllers
@@ -138,7 +139,39 @@ namespace QuangPhuWeb.Areas.Customer.Controllers
             }
             if (applicationUser.CompanyId.GetValueOrDefault() == 0)
             {
-                //Customer 
+
+                var domain = "https://localhost:7147/";
+                var options = new Stripe.Checkout.SessionCreateOptions
+                {
+                    SuccessUrl = domain + $"customer/cart/OrderConfirmation?id={ShoppingCartVM.OrderHeader.Id}",
+                    CancelUrl = domain + "customer/cart/index",
+                    LineItems = new List<Stripe.Checkout.SessionLineItemOptions>(),
+                    
+                    Mode = "payment",
+                };
+                foreach(var item in ShoppingCartVM.ShoppingCartList)
+                {
+                    var sessionLineItem = new SessionLineItemOptions
+                    {
+                        PriceData = new SessionLineItemPriceDataOptions
+                        {
+                            UnitAmount = (long)(item.Price * 100), //$20*50 => 2050
+                            Currency = "usd",
+                            ProductData = new SessionLineItemPriceDataProductDataOptions
+                            {
+                                Name = item.Product.Title,
+                            }
+                        },
+                        Quantity = item.Count
+                    };
+                    options.LineItems.Add(sessionLineItem);
+                }
+                var service = new SessionService();
+                Session session =  service.Create(options);
+                _unitofWork.OrderHeader.UpdateStripePaymentID(ShoppingCartVM.OrderHeader.Id, session.Id, session.PaymentIntentId);
+                _unitofWork.Save();
+                Response.Headers.Add("Location", session.Url);
+                return new StatusCodeResult(303); 
             }
             return RedirectToAction(nameof(OrderConfirmation), new {id = ShoppingCartVM.OrderHeader.Id});
 		}
