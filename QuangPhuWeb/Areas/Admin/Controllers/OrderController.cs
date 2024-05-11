c9f19e84ef1ceee197ee69a2500494a43f0a5859
quangphu <trancongquangphu10@gmail.com>
2024-05-11 16:27:55 +0700

    Filter in order list 11/5

--- QuangPhuWeb/Areas/Admin/Controllers/OrderController.cs
+++ QuangPhuWeb/Areas/Admin/Controllers/OrderController.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using QuangPhu.DataAccess.Repository.IRepository;
 using QuangPhu.Models;
+using QuangPhu.Models.ViewModels;
+using QuangPhu.Utility;
 
 namespace QuangPhuWeb.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class OrderController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
@@ -11,10 +14,31 @@ namespace QuangPhuWeb.Areas.Admin.Controllers
         {
             _unitOfWork = unitOfWork;
         }
-        public IActionResult Index()
+        public IActionResult Index(string status)
         {
-            return View();
+            List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            switch (status)
+            {
+                case "pending":
+                    objOrderHeaders = objOrderHeaders.Where(x => x.PaymentStatus == SD.PaymentStatusPending).ToList();
+                    break;
+                case "inprocess":
+                    objOrderHeaders = objOrderHeaders.Where(x => x.PaymentStatus == SD.StatusInProcess).ToList();
+                    break;
+                case "completed":
+                    objOrderHeaders = objOrderHeaders.Where(x => x.PaymentStatus == SD.StatusShipped).ToList();
+                    break;
+                case "approved":
+                    objOrderHeaders = objOrderHeaders.Where(x => x.PaymentStatus == SD.StatusApproved).ToList();
+                    break;
+                default:
+                    break;
+            }
+            return View(objOrderHeaders);
         }
+
+
+
         #region API CALLS
         [HttpGet]
         public IActionResult GetAll()
@@ -22,5 +46,6 @@ namespace QuangPhuWeb.Areas.Admin.Controllers
             List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
             return Json(new {data = objOrderHeaders});
         }
+        #endregion
     }
 }
